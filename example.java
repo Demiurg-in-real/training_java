675baea60cd64f551f0c7133c5c0e8744739477e
Demiurg-in-real <bezginandrew@gmail.com>
2019-03-26 05:53:24 -0400

    Ai

--- example.java
+++ example.java
@@ -1,6 +1,12 @@
 class example {
 	public static void main(String args[])
 	{
+		int perem=500;
 		System.out.println("rabotaet");
+		for (int i=0; i<1000; i++)
+		{
+			perem+=i;
+		}
+		System.out.println("Vah, maladec "+perem+" tochno!");
 	}
 }
